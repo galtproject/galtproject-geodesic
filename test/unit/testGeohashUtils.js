2c13cc1c68ee8f1aaf863e1f8a8b59dbade381b7
Nikita Chebykin <nikita@chebyk.in>
2019-08-29 13:41:27 +0200

    Add geohash5z format

--- test/unit/testGeohashUtils.js
+++ test/unit/testGeohashUtils.js
@@ -1,11 +1,14 @@
 const GeohashUtils = artifacts.require('./utils/GeohashUtils.sol');
 const MockGeohashUtils = artifacts.require('./mocks/MockGeohashUtils.sol');
 
-const { assertRevert } = require('../helpers');
+GeohashUtils.numberFormat = 'String';
 
-contract('LandUtils', ([deployer]) => {
+const { assertRevert, numberToEvmWord } = require('../helpers');
+
+contract('GeoHashUtils', ([deployer]) => {
   beforeEach(async function() {
     this.utils = await GeohashUtils.new({ from: deployer });
+    this.geohashUtils = this.utils;
 
     MockGeohashUtils.link('GeohashUtils', this.utils.address);
     this.mockGeohashUtils = await MockGeohashUtils.new({ from: deployer });
@@ -51,6 +54,78 @@ contract('LandUtils', ([deployer]) => {
     it('should revert if a value is greater than max', async function() {
       // max is 1152921504606846975
       await assertRevert(this.geohash5Precision('1152921504606846976'));
+    });
+  });
+
+  describe('geohash5z', () => {
+    // 0x00004627
+    const height = 17959;
+    // 0xfffbb722
+    const negativeHeight = -280798;
+    // overflowed height
+    const overflowedHeight = 2147483648;
+    // underflowed height
+    // const underflowedHeight = 4294967298;
+    // u33d9u9n4juh
+    const geohash5 = '940245506947434320';
+
+    const positiveGeohash5z = web3.utils.hexToNumberString(
+      '0x0000000000000000000000000000000000004627000000000d0c6c4e93424750'
+    );
+    const negativeGeohash5z = web3.utils.hexToNumberString(
+      '0x00000000000000000000000000000000fffbb722000000000d0c6c4e93424750'
+    );
+
+    describe('#geohash5ToGeohash5z()', () => {
+      it('should encode positive height into geohash5', async function() {
+        const res = await this.geohashUtils.geohash5ToGeohash5z(height, geohash5);
+        assert.equal(numberToEvmWord(res), '0x0000000000000000000000000000000000004627000000000d0c6c4e93424750');
+      });
+
+      it('should encode positive height into geohash5 bytes32', async function() {
+        const res = await this.geohashUtils.geohash5ToGeohash5zBytes32(height, geohash5);
+        assert.equal(res, '0x0000000000000000000000000000000000004627000000000d0c6c4e93424750');
+      });
+
+      it('should encode negative height into geohash5', async function() {
+        const res = await this.geohashUtils.geohash5ToGeohash5zBytes32(negativeHeight, geohash5);
+        assert.equal(numberToEvmWord(res), '0x00000000000000000000000000000000fffbb722000000000d0c6c4e93424750');
+      });
+
+      it('should encode negative height into geohash5 bytes32', async function() {
+        const res = await this.geohashUtils.geohash5ToGeohash5zBytes32(negativeHeight, geohash5);
+        assert.equal(res, '0x00000000000000000000000000000000fffbb722000000000d0c6c4e93424750');
+      });
+
+      it('should encode positive height into geohash5', async function() {
+        await assertRevert(this.geohashUtils.geohash5ToGeohash5z(overflowedHeight, geohash5));
+      });
+    });
+
+    describe('#geohash5zToGeohash5()', () => {
+      it('should decode positive height and geohash5', async function() {
+        const res = await this.geohashUtils.geohash5zToGeohash(positiveGeohash5z);
+        assert.equal(res.height, height);
+        assert.equal(res.geohash5, geohash5);
+      });
+
+      it('should decode positive height and geohash5 in bytes32 format', async function() {
+        const res = await this.geohashUtils.geohash5zToGeohashBytes32(positiveGeohash5z);
+        assert.equal(res.height, '0x0000000000000000000000000000000000000000000000000000000000004627');
+        assert.equal(res.geohash5, '0x0000000000000000000000000000000000000000000000000d0c6c4e93424750');
+      });
+
+      it('should decode negative height and geohash5', async function() {
+        const res = await this.geohashUtils.geohash5zToGeohash(negativeGeohash5z);
+        assert.equal(res.height, negativeHeight);
+        assert.equal(res.geohash5, geohash5);
+      });
+
+      it('should decode negative height and geohash5 in bytes32 format', async function() {
+        const res = await this.geohashUtils.geohash5zToGeohashBytes32(negativeGeohash5z);
+        assert.equal(res.height, '0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbb722');
+        assert.equal(res.geohash5, '0x0000000000000000000000000000000000000000000000000d0c6c4e93424750');
+      });
     });
   });
 });
